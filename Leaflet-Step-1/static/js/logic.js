02bc7f293d60c5f82b2fab43d20285c67c1f6986
darchoco <56795928+darchoco@users.noreply.github.com>
2020-04-04 11:02:41 -0700

    -push up legend

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,11 +1,17 @@
 var jsonUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
-
+function getColor(d) {
+  return d <= 1 ? 'green' : // Means: if (d >= 1966) return 'green' else…
+    d <= 2 ? 'yellow' : // if (d >= 1960) return 'black' else etc…
+    d <= 3 ? 'gold' :
+    d <= 4 ? 'orange' : // Note that numbers must be in descending order
+    'red';
+};
     function createMap(earthquakedata) {
 
         // Create the tile layer that will be the background of our map
         var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}", {
           attribution: "Map data &copy; <a href=\"http://openstreetmap.org\">OpenStreetMap</a> contributors, <a href=\"http://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"http://mapbox.com\">Mapbox</a>",
-          maxZoom: 5,
+          maxZoom: 10,
           id: "mapbox.streets",
           accessToken: API_KEY
         });
@@ -22,7 +28,7 @@ var jsonUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_wee
       
         // Create the map object with options
         var map = L.map("map", {
-          center: [40.73, -74.0059],
+          center: [39.8283, -98.5795],
           zoom: 5,
           layers: [streetmap, earthquakedata]
         });
@@ -31,8 +37,27 @@ var jsonUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_wee
         L.control.layers(baseMaps, overlayMaps, {
           collapsed: false
         }).addTo(map);
-      }
-      
+
+    var legend = L.control({position: 'bottomright'});
+    legend.onAdd = function (map) {
+
+      var div = L.DomUtil.create('div', 'legend');
+      labels = ['<strong>Categories</strong>'],
+      categories = ['0-1','1-2','2-3','3-4','5+'];
+  
+      for (var i = 0; i < categories.length; i++) {
+  
+              div.innerHTML += 
+              labels.push(
+                  '<i class="circle" style="background:' + getColor(i+1) + '"></i> ' +
+              (categories[i] ? categories[i] : '+'));
+  
+          }
+          div.innerHTML = labels.join('<br>');
+      return div;
+      };
+      legend.addTo(map);
+    };
       function createMarkers(data) {
       
       
@@ -43,10 +68,18 @@ var jsonUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_wee
         // Loop through the stations array
         for (var index = 0; index < data.features.length; index++) {
             var location = data.features[index];
-      
+            var utcSeconds = location.properties.time
+            var d = new Date(0)
+            d.setUTCSeconds(utcSeconds);
           // For each station, create a marker and bind a popup with the station's name
-          var quakeMarker = L.circle([location.geometry.coordinates[1], location.geometry.coordinates[0]],location.properties.mag*10000)
-          .bindPopup("<h3>" + location.properties.place + "</h3><h3>Magnitude: " + location.properties.mag + "<h3>");
+          var quakeMarker = L.circleMarker([location.geometry.coordinates[1], location.geometry.coordinates[0]],
+          {
+            color: getColor(location.properties.mag),
+            fillColor: getColor(location.properties.mag),
+            fillOpacity: 0.5,
+            radius: location.properties.mag*4
+          })
+          .bindPopup("<h3>" + location.properties.place + "</h3><h3>Magnitude: " + location.properties.mag + "</h3><h3>date: "+d);
     
         // Add the marker to the bikeMarkers array
         quakeMarkers.push(quakeMarker);
